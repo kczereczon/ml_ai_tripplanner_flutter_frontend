bbac6fecd0fa3b0e99c57e04ccaed56edfe4e789
Krzysztof Czereczon <kczereczon@gmail.com>
2021-05-25 19:45:28 +0200

    Twaking bottom nav bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -31,71 +31,64 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Laira",
-              style: TextStyle(
-                  color: Color(0xFF189AB4),
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text("Your travel starts here",
-                style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w300,
-                    color: Color(0xFFb49818))),
-          ],
-        ),
-        toolbarHeight: 80,
-        shadowColor: Colors.transparent,
-        backgroundColor: Colors.white,
-      ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 13.0, top: 10),
-              child: Text(
-                "Near you",
-                style: TextStyle(
-                    color: Color(0xFF189AB4),
-                    fontWeight: FontWeight.w600,
-                    fontSize: 25),
+      extendBody: true,
+      body: Container(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 13.0, top: 40),
+                child: Text(
+                  "Near you",
+                  style: TextStyle(fontWeight: FontWeight.w800, fontSize: 30),
+                ),
               ),
-            ),
-            SizedBox(height: 10),
-            PlacesList(),
-            PlacesList(),
-            PlacesList(),
-            PlacesList(),
-          ],
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(left: 13.0, top: 10, bottom: 5),
+                child: Text(
+                  "New in your neibour",
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                ),
+              ),
+              PlacesList(),
+              PlacesList(),
+              PlacesList(),
+              PlacesList(),
+            ],
+          ),
         ),
       ),
       backgroundColor: Color(0xFFEEEEEE),
-      bottomNavigationBar: BottomNavigationBar(
-        elevation: 0,
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Near you',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.map_rounded),
-            label: 'Trip planner',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.people),
-            label: 'Social',
-          ),
-        ],
-      ),
+      bottomNavigationBar: Container(
+          child: ClipRRect(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30.0),
+              topRight: Radius.circular(30.0),
+            ),
+            child: BottomNavigationBar(
+              elevation: 0,
+              currentIndex: _selectedIndex,
+              onTap: _onItemTapped,
+              selectedItemColor: Colors.black,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: 'Near you',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.map_rounded),
+                  label: 'Trip planner',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.people),
+                  label: 'Social',
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
@@ -128,17 +121,33 @@ class PlaceCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 15,
-      child: InkWell(
-        splashColor: Colors.blue.withAlpha(30),
-        onTap: () {
-          print('Card tapped.');
-        },
-        child: const SizedBox(
-          width: 180,
-          height: 220,
-          child: Text('A card that can be tapped'),
+    return Container(
+      child: Card(
+        elevation: 0,
+        child: SizedBox(
+            width: 200,
+            height: 300,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10)),
+                child: Image.network(
+                  "https://tropter.com/uploads/uploads/images/ce/b8/1df885b1d3b8bf8e6a764c7e023a54b722a7/letni_palac_lubomirskich_000_big.jpg?t=20200122105218",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Atraction",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+              )
+            ])),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
         ),
       ),
     );
