351934d0b856570cf2244703ca3a2bcf56d58767
Krzysztof Czereczon <kczereczon@gmail.com>
2021-07-20 20:51:13 +0200

    Adding planning route form

--- lib/screens/tabs/planning.dart
+++ lib/screens/tabs/planning.dart
@@ -18,12 +18,22 @@ class _PlanningState extends State<Planning> {
   final GeolocatorPlatform _geolocatorPlatform = GeolocatorPlatform.instance;
   LatLng _initialPosition;
   List<Place> _placeList;
+  List<RadioModel> _radioButtons = [];
+  RadioModel _selectedRadio;
+  double _value = 20;
+  double _time = 60;
 
   @override
   void initState() {
     super.initState();
     _getUserLocation();
     _getNearPlaces();
+
+    _radioButtons.add(new RadioModel(false, Icons.directions_car, "car"));
+    _radioButtons.add(new RadioModel(false, Icons.directions_bike, "bike"));
+    _radioButtons.add(new RadioModel(true, Icons.directions_walk, "walk"));
+
+    _selectedRadio = _radioButtons[2];
   }
 
   void _getUserLocation() async {
@@ -100,16 +110,192 @@ class _PlanningState extends State<Planning> {
       }
     }
 
+    String _minsToString(double mins) {
+      int roundedMins = mins.toInt();
+
+      int hours = roundedMins ~/ 60;
+      int trueMins = roundedMins - hours * 60;
+
+      return hours.toString() + "h " + trueMins.toString() + "m";
+    }
+
     return new Container(
-        child: MapboxMap(
-      styleString: 'mapbox://styles/mapbox/streets-v8',
-      accessToken: "",
-      onMapCreated: _onMapCreated,
-      myLocationEnabled: true,
-      initialCameraPosition: CameraPosition(
-        target: _initialPosition,
-        zoom: 14.4746,
+      child: Column(
+        children: [
+          Container(
+              height: 300,
+              child: MapboxMap(
+                styleString: 'mapbox://styles/mapbox/streets-v8',
+                accessToken: "",
+                onMapCreated: _onMapCreated,
+                myLocationEnabled: true,
+                initialCameraPosition: CameraPosition(
+                  target: _initialPosition,
+                  zoom: 14.4746,
+                ),
+              )),
+          Container(
+              height: 420,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Trip planning",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 35),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Type of vehicle",
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal, fontSize: 20),
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            InkWell(
+                              //highlightColor: Colors.red,
+                              splashColor: Colors.white70,
+                              onTap: () {
+                                setState(() {
+                                  _radioButtons.forEach(
+                                      (element) => element.isSelected = false);
+                                  _radioButtons[0].isSelected = true;
+                                });
+                              },
+                              child: new RadioItem(_radioButtons[0]),
+                            ),
+                            SizedBox(width: 20),
+                            InkWell(
+                              //highlightColor: Colors.red,
+                              splashColor: Colors.white70,
+                              onTap: () {
+                                setState(() {
+                                  _radioButtons.forEach(
+                                      (element) => element.isSelected = false);
+                                  _radioButtons[1].isSelected = true;
+                                });
+                              },
+                              child: new RadioItem(_radioButtons[1]),
+                            ),
+                            SizedBox(width: 20),
+                            InkWell(
+                              //highlightColor: Colors.red,
+                              splashColor: Colors.white70,
+                              onTap: () {
+                                setState(() {
+                                  _radioButtons.forEach(
+                                      (element) => element.isSelected = false);
+                                  _radioButtons[2].isSelected = true;
+                                });
+                              },
+                              child: new RadioItem(_radioButtons[2]),
+                            )
+                          ]),
+                      SizedBox(height: 10),
+                      Text(
+                        "How for you want to go",
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal, fontSize: 20),
+                      ),
+                      Column(
+                        children: [
+                          SliderTheme(
+                            data: SliderThemeData(
+                                thumbColor: Colors.white,
+                                thumbShape: RoundSliderThumbShape(
+                                    enabledThumbRadius: 10)),
+                            child: Slider(
+                              max: 150,
+                              value: _value,
+                              onChanged: (val) {
+                                _value = val;
+                                setState(() {});
+                              },
+                            ),
+                          ),
+                          Text(_value.toInt().toString() + " km")
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "How much time you have to spand",
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal, fontSize: 20),
+                      ),
+                      Column(
+                        children: [
+                          SliderTheme(
+                            data: SliderThemeData(
+                                thumbColor: Colors.white,
+                                thumbShape: RoundSliderThumbShape(
+                                    enabledThumbRadius: 10)),
+                            child: Slider(
+                              max: 180,
+                              value: _time,
+                              onChanged: (val) {
+                                _time = val;
+                                setState(() {});
+                              },
+                            ),
+                          ),
+                          Text(_minsToString(_time))
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: 50,
+                        child: TextButton(
+                          onPressed: () async {},
+                          child: Text("Lets find a trip!",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w300)),
+                          style: TextButton.styleFrom(
+                            backgroundColor: Colors.blueAccent,
+                          ),
+                        ),
+                      )
+                    ]),
+              ))
+        ],
       ),
-    ));
+    );
   }
+}
+
+class RadioItem extends StatelessWidget {
+  final RadioModel _item;
+  RadioItem(this._item);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 80,
+        height: 80,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
+            ),
+            color: _item.isSelected ? Colors.blueAccent : Colors.white),
+        child: Icon(
+          _item.icon,
+          color: _item.isSelected ? Colors.white : Colors.black,
+        ));
+  }
+}
+
+class RadioModel {
+  bool isSelected;
+  final IconData icon;
+  final String name;
+
+  RadioModel(this.isSelected, this.icon, this.name);
 }
