cfac7bd34ef4fa931f82cb22c98848659ad0c1a9
Krzysztof Czereczon <kczereczon@gmail.com>
2021-11-14 13:18:27 +0100

    Added way to submit comment

--- lib/screens/places/detail.dart
+++ lib/screens/places/detail.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
@@ -5,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:laira/entities/place.dart';
 import 'package:laira/utils/constant.dart';
 import 'package:laira/utils/uses-api.dart';
+import 'package:http/http.dart' as http;
 
 final storage = new FlutterSecureStorage();
 
@@ -31,179 +34,6 @@ class _PlaceDetailState extends State<PlaceDetail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-        // body: Container(
-        //   decoration: BoxDecoration(
-        //       color: Color(0xfff7f7f7),
-        //       image: DecorationImage(
-        //           image: NetworkImage(widget.place!.photoUrl),
-        //           fit: BoxFit.fitHeight,
-        //           alignment: Alignment.topCenter)),
-        //   child: SingleChildScrollView(
-        //     child: Padding(
-        //         padding: EdgeInsets.only(top: 500),
-        //         child: Container(
-        //           decoration: BoxDecoration(
-        //             borderRadius: BorderRadius.only(
-        //                 topLeft: Radius.circular(25),
-        //                 topRight: Radius.circular(25)),
-        //             color: Color(0xfff7f7f7),
-        //           ),
-        //           child: Padding(
-        //             padding: const EdgeInsets.all(16.0),
-        //             child: Column(
-        //                 crossAxisAlignment: CrossAxisAlignment.start,
-        //                 children: [
-        //                   Text(
-        //                     widget.place!.name,
-        //                     style: TextStyle(
-        //                         fontWeight: FontWeight.bold, fontSize: 35),
-        //                   ),
-        //                   Text(
-        //                     widget.place!.address.getAddressOnUi(),
-        //                     style: TextStyle(
-        //                         fontWeight: FontWeight.w300, fontSize: 23),
-        //                   ),
-        //                   Padding(
-        //                     padding: const EdgeInsets.only(top: 16.0),
-        //                     child: Text(
-        //                       widget.place!.description,
-        //                       style: TextStyle(
-        //                           fontWeight: FontWeight.w200, fontSize: 18),
-        //                     ),
-        //                   ),
-        //                   SizedBox(
-        //                     height: 30,
-        //                   ),
-        //                   widget.place!.getRating(20),
-        //                   SizedBox(
-        //                     height: 10,
-        //                   ),
-        //                   Card(
-        //                     elevation: 0,
-        //                     color: Colors.transparent,
-        //                     child: Padding(
-        //                       padding: const EdgeInsets.all(10.0),
-        //                       child: Row(
-        //                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        //                         children: [
-        //                           Column(
-        //                             children: [
-        //                               Icon(
-        //                                 Icons.directions_walk,
-        //                                 size: 30,
-        //                               ),
-        //                               Text(widget.place!.walkTime())
-        //                             ],
-        //                           ),
-        //                           Column(
-        //                             children: [
-        //                               Icon(
-        //                                 Icons.directions_bike,
-        //                                 size: 30,
-        //                               ),
-        //                               Text(widget.place!.bikeTime())
-        //                             ],
-        //                           ),
-        //                           Column(
-        //                             children: [
-        //                               Icon(
-        //                                 Icons.directions_car,
-        //                                 size: 30,
-        //                               ),
-        //                               Text(widget.place!.carTime())
-        //                             ],
-        //                           )
-        //                         ],
-        //                       ),
-        //                     ),
-        //                   ),
-        //                   Padding(
-        //                     padding: const EdgeInsets.only(top: 30.0, bottom: 20),
-        //                     child: Row(
-        //                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //                       children: [
-        //                         Padding(
-        //                           padding: const EdgeInsets.only(right: 16.0),
-        //                           child: CircleAvatar(
-        //                             backgroundImage: NetworkImage(
-        //                                 "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Kayaking_off_Na_Pali_coast.jpg/1200px-Kayaking_off_Na_Pali_coast.jpg"),
-        //                           ),
-        //                         ),
-        //                         Expanded(
-        //                             child: TextField(
-        //                           decoration: InputDecoration(
-        //                             filled: true,
-        //                             fillColor: Colors.white,
-        //                             hintText: "Write comment",
-        //                             suffixIcon: Icon(Icons.send),
-        //                             border: OutlineInputBorder(
-        //                               borderRadius: BorderRadius.circular(10),
-        //                               borderSide: BorderSide(
-        //                                 width: 0,
-        //                                 style: BorderStyle.none,
-        //                               ),
-        //                             ),
-        //                           ),
-        //                         )),
-        //                       ],
-        //                     ),
-        //                   ),
-        //                   Padding(
-        //                     padding: const EdgeInsets.only(top: 8.0),
-        //                     child: Column(
-        //                       crossAxisAlignment: CrossAxisAlignment.start,
-        //                       children: [
-        //                         Row(
-        //                           // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //                           children: [
-        //                             Padding(
-        //                               padding: const EdgeInsets.only(right: 16.0),
-        //                               child: CircleAvatar(
-        //                                 backgroundImage: NetworkImage(
-        //                                     "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Kayaking_off_Na_Pali_coast.jpg/1200px-Kayaking_off_Na_Pali_coast.jpg"),
-        //                               ),
-        //                             ),
-        //                             Expanded(
-        //                               child: Column(
-        //                                 crossAxisAlignment:
-        //                                     CrossAxisAlignment.stretch,
-        //                                 mainAxisAlignment:
-        //                                     MainAxisAlignment.spaceEvenly,
-        //                                 children: [
-        //                                   Row(
-        //                                     mainAxisAlignment:
-        //                                         MainAxisAlignment.spaceBetween,
-        //                                     children: [
-        //                                       Text(
-        //                                         "John Kowalski",
-        //                                         style: TextStyle(
-        //                                             fontWeight: FontWeight.bold,
-        //                                             fontSize: 15),
-        //                                       ),
-        //                                       Text(
-        //                                         "02.03.2020",
-        //                                         style: TextStyle(
-        //                                           fontWeight: FontWeight.w300,
-        //                                           fontSize: 13,
-        //                                         ),
-        //                                       ),
-        //                                     ],
-        //                                   ),
-        //                                   Text("Best place for holyday!"),
-        //                                 ],
-        //                               ),
-        //                             )
-        //                           ],
-        //                         ),
-        //                       ],
-        //                     ),
-        //                   ),
-        //                 ]),
-        //           ),
-        //         )),
-        //   ),
-        // ),
         body: Container(
       child: SingleChildScrollView(
         child: Column(children: [
@@ -214,7 +44,7 @@ class _PlaceDetailState extends State<PlaceDetail> {
               child: CarouselSlider(
                 options: CarouselOptions(
                     autoPlay: false,
-                    aspectRatio: 16 / 9,
+                    aspectRatio: 1,
                     enlargeCenterPage: false,
                     viewportFraction: 1),
                 items: [
@@ -304,7 +134,9 @@ class _PlaceDetailState extends State<PlaceDetail> {
               ],
             ),
           ),
-          CommentWrite(),
+          CommentWrite(
+            placeId: widget.place!.id,
+          ),
           Comments()
         ]),
       ),
@@ -383,37 +215,109 @@ class Comment extends StatelessWidget {
   }
 }
 
-class CommentWrite extends StatelessWidget {
+class CommentWrite extends StatefulWidget {
   const CommentWrite({
     Key? key,
+    required this.placeId,
   }) : super(key: key);
+
+  final String placeId;
+
+  @override
+  _CommentWriteState createState() => _CommentWriteState();
+}
+
+class _CommentWriteState extends State<CommentWrite> {
+  Future<http.Response> _getUser({BuildContext? context}) async {
+    return await UsesApi.get('/api/user/details', context: context);
+  }
+
+  String _description = "";
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(left: 16.0, top: 30, right: 16.0),
-      child: Column(
-        children: [
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 5.0),
-              child: CircleAvatar(
-                radius: 28,
-                backgroundImage: NetworkImage(
-                    "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Kayaking_off_Na_Pali_coast.jpg/1200px-Kayaking_off_Na_Pali_coast.jpg"),
-              ),
+    // return Container(
+    //   padding: const EdgeInsets.only(left: 16.0, top: 30, right: 16.0),
+    //   child: Column(
+    //     children: [
+    //       Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+    //         Padding(
+    //           padding: const EdgeInsets.only(right: 5.0),
+    //           child: CircleAvatar(
+    //             radius: 28,
+    //             backgroundImage: NetworkImage(
+    //                 "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Kayaking_off_Na_Pali_coast.jpg/1200px-Kayaking_off_Na_Pali_coast.jpg"),
+    //           ),
+    //         ),
+    //         Expanded(
+    //             child: TextField(
+    //           decoration: InputDecoration(
+    //             filled: true,
+    //             hintText: "Write comment",
+    //             suffixIcon: Icon(Icons.send),
+    //           ),
+    //         )),
+    //       ])
+    //     ],
+    //   ),
+    // );
+    return FutureBuilder(
+      future: _getUser(context: context),
+      builder: (BuildContext context, AsyncSnapshot<http.Response> snapshot) {
+        if (snapshot.hasData) {
+          Map<String, dynamic> map = jsonDecode(snapshot.data!.body);
+          TextEditingController _controller = new TextEditingController();
+          _controller.addListener(() => this._description = _controller.text);
+
+          return Container(
+            padding: const EdgeInsets.only(left: 16.0, top: 30, right: 16.0),
+            child: Column(
+              children: [
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 5.0),
+                        child: CircleAvatar(
+                          radius: 28,
+                          backgroundImage: NetworkImage(map['test']
+                                      ['profilePhoto'] !=
+                                  null
+                              ? map['test']['profilePhoto']
+                              : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"),
+                        ),
+                      ),
+                      Expanded(
+                          child: TextField(
+                        controller: _controller,
+                        decoration: InputDecoration(
+                          filled: true,
+                          hintText: "Piszesz jako ${map['test']['name']}",
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.send),
+                            onPressed: () {
+                              UsesApi.post('/api/comments/places/', body: {
+                                "place_id": widget.placeId,
+                                "description": this._description,
+                                "rating": 3
+                              }).then((response) =>
+                                  setState(() => _controller.clear()));
+                            },
+                          ),
+                        ),
+                      )),
+                    ])
+              ],
             ),
-            Expanded(
-                child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                hintText: "Write comment",
-                suffixIcon: Icon(Icons.send),
-              ),
-            )),
-          ])
-        ],
-      ),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(
+              color: Theme.of(context).primaryColor,
+            ),
+          );
+        }
+      },
     );
   }
 }
